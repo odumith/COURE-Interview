240e9df21cbfc34c85b6f90d1977a19f2ce2c015
odumith <35708667+odumith@users.noreply.github.com>
2022-02-23 10:22:06 +0100

    Add files via upload

---
+++ Question1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+
+        static int score(int[]numb)
+        {
+            int count = 0;
+            for(int i=0;i<numb.Length;i++)
+            {
+                if(numb[i]%2==0)
+                {
+                    count++;
+                }else
+                {
+                    count = count + 3;
+                }
+                if(numb[i]==8)
+                {
+                    count = count + 5;
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            int[] example1 = new int[] { 1,2,3,4,5};
+            var loox =score(example1);
+            Console.WriteLine("example1 =>{ 1,2,3,4,5} => " + loox);
+
+            int[] example2 = new int[] { 15, 25, 35 };
+            loox = score(example2);
+            Console.WriteLine("example2 =>{ 15, 25, 35 } => " + loox);
+
+            int[] example3 = new int[] { 8,8 };
+            loox = score(example3);
+            Console.WriteLine("example3 =>{ 8,8 } => " + loox);
+
+            Console.ReadLine();
+        }
+    }
+}
